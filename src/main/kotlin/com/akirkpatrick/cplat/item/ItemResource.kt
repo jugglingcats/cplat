12fea13ef8cb1737697f787b2ddca396734982a3
Alfie Kirkpatrick <github@akirkpatrick.com>
2015-07-02 08:33:36 +0100

    Work on simple item page

--- src/main/kotlin/com/akirkpatrick/cplat/item/ItemResource.kt
+++ src/main/kotlin/com/akirkpatrick/cplat/item/ItemResource.kt
@@ -1,19 +1,11 @@
 package com.akirkpatrick.cplat.item
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter
-import com.fasterxml.jackson.annotation.JsonAnySetter
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonIgnore
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.annotation.Id
-import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Component
-import java.lang
-import java.util.LinkedHashMap
-import java.util.UUID
 import javax.ws.rs.GET
 import javax.ws.rs.POST
 import javax.ws.rs.Path
+import javax.ws.rs.PathParam
 
 Component
 Path("/item")
@@ -23,6 +15,11 @@ public open class ItemResource Autowired constructor(val repository: ItemReposit
     }
 
     GET fun list() : List<Item> {
-        return repository.findAll();
+        return repository.findAll()
     }
+
+    GET Path("{itemId}") fun getItem(PathParam("itemId") itemId: java.lang.String) : Item {
+        return repository.findOne(itemId)
+    }
+
 }
--- src/main/webapp/item/item.js
+++ src/main/webapp/item/item.js
@@ -1,11 +1,35 @@
 (function() {
     var app=angular.module("item", []);
 
-    app.controller("ItemDashboardCtrl", function($scope, $http) {
+    app.factory("ItemService", function($http, $q) {
+        var def=$q.defer();
         $http.get("/api/item").success(function(d) {
+            def.resolve(d);
+        }).error(function(e) {
+            def.cancel(e);
+        });
+        return {
+            'list': function() {
+                return def.promise;
+            },
+            'get': function(id) {
+                return $http.get("/api/item/"+id);
+            }
+        }
+    });
+
+    app.controller("ItemDashboardCtrl", function($scope, ItemService) {
+        ItemService.list().then(function(d) {
             $scope.items=d;
         });
+    });
 
+    app.controller("ItemDetailCtrl", function($scope, $stateParams, $sce, ItemService) {
+        var itemId = $stateParams.itemId;
+        ItemService.get(itemId).success(function(d) {
+            $scope.item=d;
+            $scope.content=$sce.trustAsHtml(d.content);
+        });
     });
 
     app.controller("ItemCreateCtrl", function($scope, $state, $http) {
